36f1b98c818a0b429cace2a4ddc87523cc5dd6ca
98sunny <sunny.singh@cgi.com>
2022-07-29 00:36:45 -0400

    passing specs and failing specs

--- calculator.spec.js
+++ calculator.spec.js
@@ -5,14 +5,25 @@ describe('Calculator.js',function(){
         calculator.add(5);
         //expect total to be 5
         expect(calculator.total).toBe(5);
+        expect(10).toBe(19);
     });
     it('should subtract number from the total variable',function(){
-
+        const calculator = new Calculator();
+        calculator.total = 50;
+        calculator.subtract(10);
+        expect(calculator.total).toBe(40);
     });
     it('should multiply number from the total variable',function(){
+        const calculator = new Calculator();
+        calculator.total =5;
+        calculator.multiply(5);
+        expect(calculator.total).toBe(25)
 
     });
     it('should divide total variable',function(){
-
+        calculator = new Calculator();
+        calculator.total=10;
+        calculator.divide(2);
+        expect(calculator.total).toBe(5);
     });
 });
